cddbd4a95e6445f6396535820dc62787936bc6cf
Mifa43 <mifa43kotez@gmail.com>
2021-12-06 12:41:53 +0100

    kreiranje endpointa

--- src/main.py
+++ src/main.py
@@ -9,12 +9,39 @@ logger.setLevel("DEBUG")
 app = FastAPI()
 
 @app.get("/")
-async def helth_check():
-    el = ElasticClass().createindex()
-    print(el)
-    index = ElasticClass().indexExists()
-    print(index)
+async def helth_check():    
     return {"Helth": "OK"}
 
+@app.post("/create-index")
+async def create_index():
+    el = ElasticClass().createIndex()
+    print(el)
+
+    return {"messasge": "index created"}
+
+@app.delete("/delete-index")
+async def delete_index():
+    delete = ElasticClass().deleteIndex()
+    print(delete) 
+
+    return {"message": f"deleted index {delete}"}
+
+@app.get("/get-index")
+async def get_index():
+    index = ElasticClass().getIndex()
+    print(index)
+    return {"message": "index found"}
+
+@app.post("/bulk")
+async def bulk_insert():
+    bulk = ElasticClass().bulkInsert()
+    print(bulk)
+    return {"message": "data inserted"}
+
+@app.get("/search")
+async def search():
+    search_data = ElasticClass().searchData()
+    print(search_data)
+    return {"query_result": f"{search_data}"}
 if __name__ == "__main__":
     uvicorn.run(app, port=8080, loop="asyncio")
