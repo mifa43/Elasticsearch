e22edf76d24f7ab3cec2cbacc9651f3c6319cd18
Mifa43 <mifa43kotez@gmail.com>
2021-12-17 09:54:32 +0100

    novi data modeli

--- src/models.py
+++ src/models.py
@@ -1,5 +1,25 @@
 from pydantic import BaseModel
 from typing import Optional
 
+class CreateIndexModel(BaseModel):
+    name: str
+    id: int
+    doc: dict
+    alias: str
+
+class DeleteIndexModel(BaseModel):
+    name: str
+
+class IndexExistsModel(BaseModel):
+    name: str
+
+class GetDocument(BaseModel):
+    name: str
+    id: int
+class UpdateDocument(BaseModel):
+    name: str
+    id: int
+    doc: dict
+    
 class SearchModels(BaseModel):
     model: Optional[str]
