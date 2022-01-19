10f15290ff476afe60ab104df8f165fda506bea5
Mifa43 <mifa43kotez@gmail.com>
2022-01-19 10:55:38 +0100

    u job je doda definicija za citanje i salje requesta na endpoint za bulk

--- job/src/jobs.py
+++ job/src/jobs.py
@@ -1,3 +1,4 @@
+from http.client import responses
 import requests
 import pandas as pad
 import csv
@@ -12,23 +13,58 @@ import csv
 #     writer.writerow(data)
 #     file.close()
 
+# def send_request_document_bulk() -> str:
+#     """
+#     Definicija koja cita csv fajl i salje request na api gde se upisuje u elastic
+#     """
+#     df = pad.read_csv("/korisnik.csv")
+#     l = []
+    
+#     for i in range(len(df["ime"])):
+#         print(df["ime"][i])
+#         l.append({
+#         "ime":f"{df['ime'][i]}",
+#         "prezime":f"{df['prezime'][i]}",
+#         "godine":f"{df['godine'][i]}",
+#         "grad":f"{df['grad'][i]}",
+#         "drzava":f"{df['drzava'][i]}"
+        
+#         })
+#     print(l)
+#     url = 'http://fastapi-es:8080/create-document-bulk-job'
+#     data = {"indices": "2", "document": l}
+#     requests.post(url, json = data)
+#send_request_document_bulk()
 
-def send_request():
-    df = pad.read_csv("korisnik.csv")
+def send_request_helpers_bulk() -> str:
+    """
+    Definicija koja cita csv fajl i salje request na api gde se upisuje u elastic
+    """
+    df = pad.read_csv("nike_2020_04_13.csv")
     l = []
     
-    for i in range(len(df["ime"])):
-        print(df["ime"][i])
+    for i in range(len(df["Product Name"])):
+        print(df["Product Name"][i])
         l.append({
-        "ime":f"{df['ime'][i]}",
-        "prezime":f"{df['prezime'][i]}",
-        "godine":f"{df['godine'][i]}",
-        "grad":f"{df['grad'][i]}",
-        "drzava":f"{df['drzava'][i]}"
+        "URL":f"{df['URL'][i]}",
+        "Product Name":f"{df['Product Name'][i]}",
+        "Product ID":f"{df['Product ID'][i]}",
+        "Listing Price":f"{df['Listing Price'][i]}",
+        "Sale Price":f"{df['Sale Price'][i]}",
+        "Discount":f"{df['Discount'][i]}",
+        "Brand":f"{df['Brand'][i]}",
+        "Description":f"{df['Description'][i]}",
+        "Rating":f"{df['Rating'][i]}",
+        "Reviews":f"{df['Reviews'][i]}",
+        "Images":f"{df['Images'][i]}",
+        "Last Visited":f"{df['Last Visited'][i]}",
         
         })
     print(l)
-    url = 'http://fastapi-es:8080/create-document-bulk-job'
-    data = {"indices": "2", "document": l}
+    url = 'http://fastapi-es:8080/bulk'
+    data = {"indices": "products", "document": l}
     requests.post(url, json = data)
-send_request()
+#send_request_helpers_bulk()
+if __name__ == "__main__":
+    send_request_helpers_bulk()
+#URL,Product Name,Product ID,Listing Price,Sale Price,Discount,Brand,Description,Rating,Reviews,Images,Last Visited
