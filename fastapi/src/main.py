0cf56131c1f008c95c06efb3c8478fc4fe8d31bb
Mifa43 <mifa43kotez@gmail.com>
2022-01-29 10:54:11 +0100

    dodat je logging za konzolu i format pisanja poruka

--- fastapi/src/main.py
+++ fastapi/src/main.py
@@ -5,14 +5,30 @@ import logging, uvicorn
 from crud import ElasticClass
 from models import *
 
-logger = logging.getLogger(__name__)
+# kreiranje logera https://docs.python.org/3/library/logging.html
+logger = logging.getLogger(__name__) 
 logger.setLevel("DEBUG")
+
+# kreiranje logger consol
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# format
+formatter = logging.Formatter('%(levelname)s:     %(name)s.%(funcName)s: %(message)s')
+
+# dodaj format u consol-u
+ch.setFormatter(formatter)
+
+# dodaj consol-u logger
+logger.addHandler(ch)
 
 app = FastAPI()
 
 @app.get("/")
 async def helth_check():
-    return {"Helth": "OK"}
+    logger.info("{Health : OK}, 200")
+
+    return {"Health": "OK"}
 
 @app.post("/create-index")
 async def create_index(index: CreateIndexModel):
